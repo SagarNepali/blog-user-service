4bd6a389a8eea84d3a0891a17bd00dff17e6ce6d
Sagar <worthless.dumboo@gmail.com>
2022-06-19 21:49:38 -0500

    Eureka discovery added

--- src/main/java/edu/miu/blogpost/BlogPostApplication.java
+++ src/main/java/edu/miu/blogpost/BlogPostApplication.java
@@ -2,8 +2,10 @@ package edu.miu.blogpost;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BlogPostApplication {
 
 	public static void main(String[] args) {
